586a54fea8d46d32d252502c72421629d96418dc
Chris-Zou <zou_pp@outlook.com>
2016-05-26 00:11:27 +0800

    fix some bugs

--- algorithm/leetcode/reverse_vowels_string.go
+++ algorithm/leetcode/reverse_vowels_string.go
@@ -7,10 +7,13 @@ import (
 func main() {
 	fmt.Println(reverseVowels("hello"))
 	fmt.Println(reverseVowels("leetcode"))
+	fmt.Println(reverseVowels("aA"))
 }
 
 func isVowels(ch byte) bool {
 	if ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' {
+		return true
+	} else if ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' {
 		return true
 	} else {
 		return false
