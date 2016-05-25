510647be60ab51ef58c61e1bbc9d6100e9cedec2
Chris-Zou <zou_pp@outlook.com>
2016-05-26 00:06:47 +0800

    fix bugs

--- algorithm/leetcode/reverse_vowels_string.go
+++ algorithm/leetcode/reverse_vowels_string.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	fmt.Println(reverseVowels("hello"))
+	fmt.Println(reverseVowels("leetcode"))
 }
 
 func isVowels(ch byte) bool {
@@ -20,15 +21,25 @@ func isVowels(ch byte) bool {
 func reverseVowels(s string) string {
 	l := len(s)
 	i, j := 0, l-1
+
+	ret := make([]byte, l)
+	for k := 0; k < l; k++ {
+		ret[k] = s[k]
+	}
 	for i < j {
 		for i < j && isVowels(s[i]) == false {
+
 			i++
 		}
 		for i < j && isVowels(s[j]) == false {
+
 			j--
 		}
 		if i < j {
-			s[i], s[j] = s[j], s[i]
+			ret[i], ret[j] = s[j], s[i]
+			i++
+			j--
 		}
 	}
+	return string(ret)
 }
