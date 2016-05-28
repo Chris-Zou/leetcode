3e8f6afc2b513d09aa007e08d59b3bff36adbaeb
Chris-Zou <zou_pp@outlook.com>
2016-05-29 00:24:13 +0800

    add a new method in tenth_line.sh

--- leetcode/shell/tenth_line.sh
+++ leetcode/shell/tenth_line.sh
@@ -1,4 +1,5 @@
 # Read from the file file.txt and output the tenth line to stdout.
 #awk 'NR == 10' file.txt #using awk, accepted
 #sed -n '10p' file.txt   #using sed, accepted
-tail -n+10 file.txt | head -n1 #using head and tail
+#tail -n+10 file.txt | head -n1 #using head and tail, accepted
+head -n10 file2.txt | tail -n+10 #using head and tail, accepted
