94089784c5d13639f9c92f1b2b45bace6fc96ade
Chris-Zou <zou_pp@outlook.com>
2016-05-29 00:13:35 +0800

    finished tenth_line.sh

---
+++ leetcode/shell/tenth_line.sh
@@ -0,0 +1,4 @@
+# Read from the file file.txt and output the tenth line to stdout.
+#awk 'NR == 10' file.txt #using awk, accepted
+#sed -n '10p' file.txt   #using sed, accepted
+tail -n+10 file.txt | head -n1 #using head and tail
