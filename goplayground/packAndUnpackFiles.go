ab139ff73956d799b82c8ebd1e4c95428ea19904
Chris-Zou <zou_pp@outlook.com>
2016-06-07 10:02:48 +0800

    fix bugs in packAndUnpackFiles.go

--- goplayground/packAndUnpackFiles.go
+++ goplayground/packAndUnpackFiles.go
@@ -14,15 +14,15 @@ import (
 func main() {
 	TarFile := "test.tar.gz"
 	src := "test.txt"
-	//dstDir := "test_ext"
+	dstDir := "test_ext"
 
 	if err := Tar(src, TarFile, false); err != nil {
 		fmt.Println(err)
 	}
 
-	/*if err := UnTar(TarFile, dstDir); err != nil {
+	if err := UnTar(TarFile, dstDir); err != nil {
 		fmt.Println(err)
-	}*/
+	}
 }
 
 func Tar(src string, dstTar string, failIfExist bool) (err error) {
@@ -59,6 +59,8 @@ func Tar(src string, dstTar string, failIfExist bool) (err error) {
 			fmt.Println("close file failed")
 			err = er
 		}
+		gw.Close()
+		fw.Close()
 	}()
 
 	fi, er := os.Stat(src)
@@ -113,12 +115,9 @@ func tarDir(srcBase, srcRelative string, tw *tar.Writer, fi os.FileInfo) (err er
 	return nil
 }
 
-// 因为要在 defer 中关闭文件，所以要单独创建一个函数
 func tarFile(srcBase, srcRelative string, tw *tar.Writer, fi os.FileInfo) (err error) {
-	// 获取完整路径
 	srcFull := srcBase + srcRelative
 
-	// 写入文件信息
 	hdr, err := tar.FileInfoHeader(fi, "")
 	if err != nil {
 		fmt.Println("line 124:", err)
@@ -131,7 +130,6 @@ func tarFile(srcBase, srcRelative string, tw *tar.Writer, fi os.FileInfo) (err e
 		return err
 	}
 
-	// 打开要打包的文件，准备读取
 	fr, err := os.Open(srcFull)
 	if err != nil {
 		fmt.Println("line 137:", err)
@@ -139,7 +137,6 @@ func tarFile(srcBase, srcRelative string, tw *tar.Writer, fi os.FileInfo) (err e
 	}
 	defer fr.Close()
 
-	// 将文件数据写入 tw 中
 	if _, err = io.Copy(tw, fr); err != nil {
 		fmt.Println("line 144:", err)
 		return err
@@ -148,62 +145,51 @@ func tarFile(srcBase, srcRelative string, tw *tar.Writer, fi os.FileInfo) (err e
 }
 
 func UnTar(srcTar string, dstDir string) (err error) {
-	// 清理路径字符串
 	dstDir = path.Clean(dstDir) + string(os.PathSeparator)
 
-	// 打开要解包的文件
 	fr, err := os.Open(srcTar)
 	if err != nil {
 		return err
 	}
-	defer fr.Close()
 
 	gr, err := gzip.NewReader(fr)
 
-	// 创建 tar.Reader，准备执行解包操作
 	tr := tar.NewReader(gr)
 
-	// 遍历包中的文件
 	for hdr, err := tr.Next(); err != io.EOF; hdr, err = tr.Next() {
 		if err != nil {
 			return err
 		}
 
-		// 获取文件信息
 		fi := hdr.FileInfo()
 
-		// 获取绝对路径
 		dstFullPath := dstDir + hdr.Name
 
 		if hdr.Typeflag == tar.TypeDir {
-			// 创建目录
 			os.MkdirAll(dstFullPath, fi.Mode().Perm())
-			// 设置目录权限
 			os.Chmod(dstFullPath, fi.Mode().Perm())
 		} else {
-			// 创建文件所在的目录
 			os.MkdirAll(path.Dir(dstFullPath), os.ModePerm)
-			// 将 tr 中的数据写入文件中
 			if er := unTarFile(dstFullPath, tr); er != nil {
 				return er
 			}
-			// 设置文件权限
 			os.Chmod(dstFullPath, fi.Mode().Perm())
 		}
 	}
+
+	gr.Close()
+	fr.Close()
+
 	return nil
 }
 
-// 因为要在 defer 中关闭文件，所以要单独创建一个函数
 func unTarFile(dstFile string, tr *tar.Reader) error {
-	// 创建空文件，准备写入解包后的数据
 	fw, err := os.Create(dstFile)
 	if err != nil {
 		return err
 	}
 	defer fw.Close()
 
-	// 写入解包后的数据
 	_, err = io.Copy(fw, tr)
 	if err != nil {
 		return err
@@ -212,19 +198,16 @@ func unTarFile(dstFile string, tr *tar.Reader) error {
 	return nil
 }
 
-// 判断档案是否存在
 func Exists(name string) bool {
 	_, err := os.Stat(name)
 	return err == nil || os.IsExist(err)
 }
 
-// 判断文件是否存在
 func FileExists(filename string) bool {
 	fi, err := os.Stat(filename)
 	return (err == nil || os.IsExist(err)) && !fi.IsDir()
 }
 
-// 判断目录是否存在
 func DirExists(dirname string) bool {
 	fi, err := os.Stat(dirname)
 	return (err == nil || os.IsExist(err)) && fi.IsDir()
